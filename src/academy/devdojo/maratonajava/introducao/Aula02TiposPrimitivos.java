fd9df2c9e54d146f770274fa3ab69c0a61a55104
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-04-18 19:32:31 -0300

    Aprendendo sobre tipos primitos + String (tipo não primitivo)

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,6 +3,22 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        
+        int idade = (int) 10000000000L;
+        double salarioDouble = 2000.0;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 10;
+        long numeroGrande = (long)155.23;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+        String nome = "Matheus Pereira";
+
+        System.out.println("A idade é: "+ idade +" anos");
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
+        System.out.println(salarioFloat);
+        System.out.println(numeroGrande);
+        System.out.println("Nome: " + nome);
     }
 }
