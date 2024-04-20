64540a190c440f74b05f53cf8143bb247b4fad8f
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-04-19 21:27:46 -0300

    Operador de adicao

---
+++ src/academy/devdojo/maratonajava/introducao/Aila04Operadores.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aila04Operadores {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int soma = num1+num2;
+
+        //Operadores matemáticos --> + - * /
+
+        System.out.println("Valor: " + num2+num1);
+        System.out.println(num2+num1);
+        System.out.println("Valor: " + soma);
+    }
+}
