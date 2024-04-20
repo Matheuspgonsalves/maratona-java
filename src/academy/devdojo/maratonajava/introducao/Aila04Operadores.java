916c3542995f6056d91ad71333eac83900549db3
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-04-19 21:41:05 -0300

    Operador resto e operadores relacionais

--- src/academy/devdojo/maratonajava/introducao/Aila04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aila04Operadores.java
@@ -2,14 +2,31 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aila04Operadores {
     public static void main(String[] args) {
-        int num1 = 10;
+        int num1 = 11;
         int num2 = 20;
         int soma = num1+num2;
+        float div = num2/(float)num1;
 
         //Operadores matemáticos --> + - * /
 
         System.out.println("Valor: " + num2+num1);
         System.out.println(num2+num1);
         System.out.println("Valor: " + soma);
+        System.out.println("Valor: " + div);
+
+        // % Operador de resto
+        int resto = 20 % 3;
+        System.out.println(resto);
+
+        // Operadores relacionais --> < > <= >= == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualQueVinte = 10 == 20;
+        boolean isDezDiferenteQueVinte = 10 != 20;
+        System.out.println("isDezMaiorQueVinte? " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte? " + isDezMenorQueVinte);
+        System.out.println("isDezIgualQueVinte? " + isDezIgualQueVinte);
+        System.out.println("isDezDiferenteQueVinte? " + isDezDiferenteQueVinte);
     }
 }
